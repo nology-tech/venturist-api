4c9efafcf3bd4c5e535c9005e34daadd6b44b318
Allan Shrestha <stevevai111@hotmail.com>
2022-03-25 16:22:35 +0000

    UserRepository and UserController created, application properties updated and pom xml updated

---
+++ src/main/java/com/nology/VenturistProxyAPI/UserController.java
@@ -0,0 +1,39 @@
+package com.nology.VenturistProxyAPI;
+
+import com.google.api.services.sqladmin.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "https://accentureclientprojecttest.web.app/")
+@RestController
+
+public class UserController {
+
+    @Autowired
+    UserRepository repository;
+    private List<User> users = new ArrayList<User>();
+
+    @PostMapping("/user")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        repository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Success, a new user has been added");
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+    }
+
+//    @DeleteMapping("/course/{id}")
+//    @Transactional
+//    public ResponseEntity<String> deleteCourse(@PathVariable String id) {
+//        repository.deleteById(Integer.parseInt(id));
+//        return ResponseEntity.status(HttpStatus.OK).body("Course Deleted");
+//    }
+}
+
