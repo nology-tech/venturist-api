eb2b3b457ddd10f084311e4cf324ddf6c95482c6
is-Lew <lewis.halder@outlook.com>
2022-03-30 09:36:12 +0100

    contacts class made

--- src/main/java/com/nology/VenturistProxyAPI/Contacts.java
+++ src/main/java/com/nology/VenturistProxyAPI/Contacts.java
@@ -1,4 +1,34 @@
 package com.nology.VenturistProxyAPI;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="contacts")
 public class Contacts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String user_ID;
+    private String contact_User_Id;
+
+    public Contacts() {
+    }
+
+    public Contacts(String user_ID, String contact_User_Id) {
+        this.user_ID = user_ID;
+        this.contact_User_Id = contact_User_Id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser_ID() {
+        return user_ID;
+    }
+
+    public String getContact_User_Id() {
+        return contact_User_Id;
+    }
 }
--- src/main/java/com/nology/VenturistProxyAPI/ContactsRepository.java
+++ src/main/java/com/nology/VenturistProxyAPI/ContactsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface ContactsRepository extends JpaRepository<Contacts, String> {
+
+    Contacts findById(int id);
 }
